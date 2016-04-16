3da4c2305ff4daae6eb2ff8c758fdd4fa8b86908
Matthew9161993 <mmoconnor2@wisc.edu>
2016-04-16 10:20:55 -0500

    Added files via upload

--- User.java
+++ User.java
@@ -6,12 +6,19 @@ public class User {
 	String address;
 	List<String> previousAddresses;
 	PaymentInfo card;
+	//Constructor
 	public User(String name,String email,String phone,String address){
 		this.name = name;
 		this.email = email;
 		this.phone = phone;
 		this.address = address;
 		previousAddresses.add(address);
+	}
+	public void changeName(String changedName){
+		name = changedName;
+	}
+	public void changeAddress(String newAddress){
+		address = newAddress;
 	}
 	public void setPayment(PaymentInfo card){
 		this.card = card;
