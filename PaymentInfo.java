d8834ce7681962d8a33349ca98d86a13b4023cd0
Matthew9161993 <mmoconnor2@wisc.edu>
2016-04-16 10:09:40 -0500

    Added files via upload

--- PaymentInfo.java
+++ PaymentInfo.java
@@ -1,4 +1,13 @@
 
 public class PaymentInfo {
-
+	int cardNumber;
+	int backNumbers;
+	double expiration;
+	String name;
+	public PaymentInfo(int cardNumber, int backNumbers, double expiration, String name){
+		this.cardNumber = cardNumber;
+		this.backNumbers = backNumbers;
+		this.expiration = expiration;
+		this.name = name;
+	}
 }
--- User.java
+++ User.java
@@ -1,4 +1,19 @@
-
+import java.util.List;
 public class User {
-
+	String name;
+	String email;
+	String phone;
+	String address;
+	List<String> previousAddresses;
+	PaymentInfo card;
+	public User(String name,String email,String phone,String address){
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+		this.address = address;
+		previousAddresses.add(address);
+	}
+	public void setPayment(PaymentInfo card){
+		this.card = card;
+	}
 }
