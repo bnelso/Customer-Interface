f0412936dd364530fb16e5c5b752bd1197124f54
NIkhil Kumar <nkumar0727@gmail.com>
2016-04-16 10:24:23 -0500

    Update ShoppingCart.java

--- ShoppingCart.java
+++ ShoppingCart.java
@@ -1,4 +1,34 @@
+import java.util.ArrayList;
 
-public class ShoppingCart {
 
+public class ShoppingCart 
+{
+	private ArrayList<Item> itemList;
+	private double totalFees;
+	
+	public ShoppingCart()
+	{}
+	
+	
+	//adds item to shopping cart
+	//increases total fees due by item's price
+	public void addItem(Item item)
+	{
+	  itemList.add(item);
+	  //update total fees by adding on the price of the item
+	}
+	
+	//removes item from shopping cart
+	//decreases total fees due by removed item's price
+	public void removeItem()
+	{
+	  //goes through shopping cart and removes specific item being searched for
+	  //update total fees by subtrating from the price of the item
+	}
+	
+	//necessary?
+	public void setFees(double fees)
+	{
+	  totalFees = fees;
+	}
 }
