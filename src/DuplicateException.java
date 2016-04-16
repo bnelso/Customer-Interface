832df6e7531ed1f92f43f0211e5a6c79b2787890
NIkhil Kumar <nkumar0727@gmail.com>
2016-04-16 10:02:45 -0500

    Delete DuplicateException.java

--- src/DuplicateException.java
+++
@@ -1,8 +0,0 @@
-
-public class DuplicateException extends RuntimeException{
-	private static final long serialVersionUID = 1L;
-
-    public DuplicateException(){
-        super("Duplicate keys not allowed.");
-    }
-}
